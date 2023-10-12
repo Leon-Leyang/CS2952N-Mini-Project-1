4416d589acd0759624348fa67d61fadb8ef087c1
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-10-12 10:31:43 -0400

    Finish Step2

--- Step2.py
+++ Step2.py
@@ -7,27 +7,6 @@ import time
 from PIL import Image
 
 
-# # Fix the seed for reproducibility
-torch.manual_seed(60)
-
-# Set the OpenAI API key
-# TODO: Delete this key before submitting
-openai.api_key = "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"
-
-# Load the model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-clip_model, preprocess = clip.load('RN50', device)
-
-# Construct the text prompts from the ImageNet classes
-URL = "https://storage.googleapis.com/download.tensorflow.org/data/imagenet_class_index.json"
-response = requests.get(URL)
-imagenet_class_index = response.json()
-all_classes = [item[1] for item in imagenet_class_index.values()]
-init_class_prompts = clip.tokenize([f"the photo contains {c}" for c in all_classes]).to(device)
-follow_up_prompt_template = "the photo, besides {}, contains {}"
-end_prompt_template = "the photo, besides {}, contains no other classes of objects"
-
-
 # Get the class predictions
 def get_class_predictions(img, num_classes=5):
     candidate_classes = []
@@ -51,7 +30,6 @@ def get_class_predictions(img, num_classes=5):
             img_logits, _ = clip_model(img, follow_up_prompt)
             best_class_idx = img_logits.argmax(dim=-1).cpu().numpy()[0]
             if best_class_idx == len(follow_up_prompt) - 1:
-                print("No more classes to add")
                 break
             best_class = current_class_candidates[best_class_idx]
             candidate_classes.append(best_class)
@@ -59,31 +37,41 @@ def get_class_predictions(img, num_classes=5):
 
 
 # Get a random image from ./data/Flickr8k_Dataset
-def get_random_image():
-    img_dir = './data/Flicker8k_Dataset'
-    image_files = os.listdir(img_dir)
-    image_file = image_files[torch.randint(len(image_files), size=(1,)).item()]
-    image_path = os.path.join(img_dir, image_file)
-    return image_file, preprocess(Image.open(image_path)).unsqueeze(0)
+def get_random_image(split='test'):
+    imgs_file = f'./data/Flickr8k_text/Flickr_8k.{split}Images.txt'
+    with open(imgs_file, 'r') as f:
+        imgs_file = f.readlines()
+    imgs_file = [img.strip() for img in imgs_file]
+    img_file = imgs_file[torch.randint(len(imgs_file), size=(1,)).item()]
+    img_path = os.path.join('./data/Flicker8k_Dataset', img_file)
+    img = preprocess(Image.open(img_path)).unsqueeze(0)
+    return img_file, img
+
+
+# Generate in context examples for predicting the caption
+def gen_examples():
+    examples = []
+    while len(examples) < 5:
+        try:
+            img_file, img = get_random_image('train')
+        except FileNotFoundError:
+            continue
+        classes = get_class_predictions(img, 5)
+        with open('./data/Flickr8k_text/Flickr8k.token.txt', 'r') as f:
+            captions = f.readlines()
+            for caption in captions:
+                if img_file in caption:
+                    actual_caption = caption.split("\t")[1]
+                    break
+        examples.append((', '.join(classes) + ':\n' + actual_caption))
+
+    return examples
 
 
 # Use the OpenAI API to generate a caption from the classes
 def pred_caption(classes):
-    context = """Generate a possible caption for the image with the specified classes:
-
-        black and white bird, seeds, hand::
-        A black and white bird eating seeds out of someone 's hand.
-
-        couple, baby, stroller, grass:
-        A couple sit on the grass with a baby and stroller.
-
-        bloodhound, Chesapeake Bay retriever, English springer, swimming trunks:
-        A dog is swimming.
-
-        """
-
-    # Append the desired class to the context to get attributes for it
-    prompt = context + f"{classes}:\n"
+    global basic_prompt
+    prompt = basic_prompt + f"\n{classes}:\n"
 
     response = openai.Completion.create(
         engine="davinci",
@@ -98,7 +86,12 @@ def pred_caption(classes):
 def gen_caption_4_image():
     # Get the top k predictions of classes for the image using CLIP
     num_classes = 5
-    img_file, img = get_random_image()
+    while True:
+        try:
+            img_file, img = get_random_image('test')
+            break
+        except FileNotFoundError:
+            continue
     top_k_classes = get_class_predictions(img, num_classes)
     print(f"Top {num_classes} predictions for {img_file}:")
     print(top_k_classes)
@@ -120,12 +113,43 @@ def gen_caption_4_image():
     print(actual_caption)
 
 
-for i in range(10):
-    if i > 2:
-        print("Waiting for 20 seconds to not exceed the OpenAI API rate limit...")
-        time.sleep(20)
-    print('Generating caption for a random image...')
-    gen_caption_4_image()
+if __name__ == '__main__':
+    # Fix the seed for reproducibility
+    torch.manual_seed(70)
+
+    # Set the OpenAI API key
+    openai.api_key = "YOUR_API_KEY"
+
+    # Load the model
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    clip_model, preprocess = clip.load('RN50', device)
+
+    # Construct the text prompts from the ImageNet classes
+    URL = "https://storage.googleapis.com/download.tensorflow.org/data/imagenet_class_index.json"
+    response = requests.get(URL)
+    imagenet_class_index = response.json()
+    all_classes = [item[1] for item in imagenet_class_index.values()]
+    init_class_prompts = clip.tokenize([f"the photo contains {c}" for c in all_classes]).to(device)
+    follow_up_prompt_template = "the photo, besides {}, contains {}"
+    end_prompt_template = "the photo, besides {}, contains no other classes of objects"
+
+    # Generate the basic prompt
+    print("Generating examples for the GPT-3 prompt...")
+    examples = gen_examples()
+    print("Examples generated successfully!\n")
+    basic_prompt = 'Generate a possible caption for the image with the specified classes:\n\n'
+    basic_prompt += '\n'.join(examples)
+
+    print("Generating captions for 10 random images...\n")
+
+    # Generate captions for 10 random images
+    for i in range(10):
+        if i > 2:
+            print("Waiting for 20 seconds to not exceed the OpenAI API rate limit...")
+            time.sleep(20)
+        print(f'Generating caption for image #{i + 1}...')
+        gen_caption_4_image()
+
 
 
 
